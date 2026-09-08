f67a7b31ea8daf39127abd64a6a426eea0b4c28d
Micah <contact_micah@yahoo.com>
2026-09-08 14:09:14 -0700

    Remove unused Databricks token variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,6 @@ variable "databricks_host" {
   type = string
 }
 
-variable "databricks_token" {
-  type      = string
-  sensitive = true
-}
-
-
-
 variable "databricks_account_id" {
   type = string
 }
